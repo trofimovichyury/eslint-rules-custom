1a3df6182fe96d3fcd31e9d44e938fa1ed8e1822
Yury Trafimovich <yury.trafimovich@holidayextras.com>
2021-11-18 09:47:01 +0100

    eslint

---
+++ index.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  globals: {
+      MyGlobal: true
+  },
+
+  env: {
+    commonjs: true,
+    es2021: true
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+      "ecmaVersion": 13
+  },
+  rules: {
+      semi: ["error", "always"],
+  }
+};
